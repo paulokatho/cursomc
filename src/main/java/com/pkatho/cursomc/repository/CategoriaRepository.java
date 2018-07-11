704b1b1747f0d84386599c85d3ef70b40f32adda
Paulo Katho <pkatho@gmail.com>
2018-07-11 14:40:43 -0300

    Criando e iniciando projeto

---
+++ src/main/java/com/pkatho/cursomc/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.pkatho.cursomc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pkatho.cursomc.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
